8b5d51d5736ebd99242ae042965732ed817f4eea
ISU_375 <alok.nayak@iserveu.in>
2024-09-05 08:09:23 +0530

    05-09-2024

--- 01.py
+++ 01.py
@@ -1,1 +1,12 @@
-print('New file created.')
+# print('New file created.')
+'''def findSum(x, y):
+ return x+y
+_num1 = 10
+a = 25
+A = 20
+_num2 = 16
+c = findSum(_num1, _num2)
+print("Sum is : ", c)'''
+for i in range(1,100):
+    print(i)
+a = None
