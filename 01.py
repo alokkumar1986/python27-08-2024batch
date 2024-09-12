65454a59516ea7adbdab59b7fab0ac5af0b2f18c
ISU_375 <alok.nayak@iserveu.in>
2024-09-12 08:38:56 +0530

    12-09-2024

--- 01.py
+++ 01.py
@@ -1,12 +1,34 @@
 # print('New file created.')
 '''def findSum(x, y):
  return x+y
-_num1 = 10
+# _num1 = 10
 a = 25
-A = 20
-_num2 = 16
-c = findSum(_num1, _num2)
+b = 20
+# _num2 = 16
+c = findSum(a, b)
 print("Sum is : ", c)'''
-for i in range(1,100):
-    print(i)
-a = None
+
+# for i in range(1,100):
+#     print(i)
+# a = None
+
+# a= 20
+# a= 30
+# print(a)
+
+
+# a='true' 
+
+# x = 0.6
+
+# print(x)
+# x = 3.9*4*(1-x)
+
+# print(x)
+
+a = 2
+b = 4
+print(a%b)
+
+print(type(a))
+print(id(a))
---
+++ 02.py
@@ -0,0 +1,6 @@
+a, b = 10, 15
+print(a)
+print(b)
+a,b = b,a
+print(a)
+print(b)
