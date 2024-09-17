b10b59e7d61d10d9afbc5a580c901dc286f91624
ISU_375 <alok.nayak@iserveu.in>
2024-09-17 08:33:23 +0530

    17-09-2024

---
+++ 07.py
@@ -0,0 +1,25 @@
+a = """This is Aptech Learning's 
+python class"""
+# print(a)
+# print(type(a))
+
+# print(a[0])
+# print(a[5])
+# slicing : 
+# a[startIndex:endIndex]
+# print(a[0:5])
+# print(a[0:])
+# print(a[5:])
+# print(a[:8])
+# print(a[:-1])
+
+# print(a[-4])
+
+#len() : It returns length of the string 
+print(len(a))
+#upper() : it converts characters into uppercase
+print(a[2:4].upper())
+#lower() : it converts characters into lowercase
+print(a[:5].lower())
+
+
---
+++ 08.py
@@ -0,0 +1,15 @@
+# input : s= 'karthi'
+# output : karthI
+s= 'karthi'
+# output= s[0:5]+s[5].upper()
+# print(output)
+#=============================================
+# str + str = str
+# str * str = str  //error
+
+# str + int = str //error
+# str * int = str
+print(s+' sahoo') #concatenate
+
+print(2*s) 
+
